d17fce69e5c2a41d59d737bb126a3a831da44953
Muhammad Azhari <azharumuhammad@gmail.com>
2026-04-18 23:04:49 +0700

    fix router

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -3,7 +3,7 @@ import './assets/main.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { BrowserRouter } from 'react-router'
+import { HashRouter } from 'react-router'
 import { App } from './app'
 
 const queryClient = new QueryClient()
@@ -11,9 +11,9 @@ const queryClient = new QueryClient()
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </QueryClientProvider>
   </StrictMode>
 )
